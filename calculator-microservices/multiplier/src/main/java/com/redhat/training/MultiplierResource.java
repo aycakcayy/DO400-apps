727b143e25f544d719e94579793e53bd51b59367
aycakcayy <your@user.email>
2022-09-08 04:53:38 -0400

    Replace Jenkinsfile

--- calculator-microservices/multiplier/src/main/java/com/redhat/training/MultiplierResource.java
+++ calculator-microservices/multiplier/src/main/java/com/redhat/training/MultiplierResource.java
@@ -13,23 +13,39 @@ import com.redhat.training.service.SolverService;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MultiplierResource implements MultiplierService {
-    final Logger log = LoggerFactory.getLogger(MultiplierResource.class);
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-    SolverService solverService;
+import javax.ws.rs.WebApplicationException;
 
-    @Inject
-    public MultiplierResource( @RestClient SolverService solverService ) {
-        this.solverService = solverService;
-    }
+import javax.ws.rs.core.Response;
+
+import org.jboss.resteasy.client.exception.ResteasyWebApplicationException;
+
+import org.junit.jupiter.api.function.Executable;
+
+import org.junit.jupiter.api.Test;
+
+import org.mockito.Mockito;
 
 
-    @GET
-    @Path("/{lhs}/{rhs}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public Float multiply(@PathParam("lhs") String lhs, @PathParam("rhs") String rhs) {
-        log.info("Multiplying {} to {}" ,lhs, rhs);
-        return solverService.solve(lhs)*solverService.solve(rhs);
-    }
-}
+@Test
+
+public void simpleMultiplication() {
+
+	 // Given
+	 
+  Mockito.when(solverService.solve("2")).thenReturn(Float.valueOf("2"));
+	 
+	    Mockito.when(solverService.solve("3")).thenReturn(Float.valueOf("3"));
+	 
+	 //    // When
+	 //
+	      Float result = multiplierResource.multiply("2", "3");
+	 
+	 //      // Then
+	 
+	        assertEquals( 6.0f, result );
+	 
+	        }
